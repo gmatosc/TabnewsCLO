ec5dddbf725dd0e7248a2ef5a87b613c7e3ae0dd
Gustavo Matos Costa <103973174+gmatosc@users.noreply.github.com>
2026-09-11 23:53:44 +0000

    Adicionei uma forma diferente de conectar o `SSL` ao `CA`, mas ainda não temos o `CA`

--- infra/database.js
+++ infra/database.js
@@ -1,25 +1,16 @@
 import { Client } from 'pg';
 
 async function query(queryObject) {
+  
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
+    ssl: getSSLValues(),
   });
 
-  console.log('Credenciais  do Postgres:', {
-     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-
-  
-  
   try {
     
     await client.connect();
@@ -41,3 +32,12 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues(){
+  if (process.env.POSTRES_CA){
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === 'development' ? false : true
+}
